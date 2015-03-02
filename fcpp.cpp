8c62f46cac97fb859dbf8cb5a33c46d965a8d760
Francisco Charte Ojeda <francisco@fcharte.com>
2015-03-02 08:52:18 +0100

    Removed fcpp.cpp file

--- fcpp.cpp
+++
@@ -1,8 +0,0 @@
-#include <Rcpp.h>
-using namespace Rcpp;
-
-// [[Rcpp::export]]
-void fcpp(NumericVector v, NumericVector t) {
-  for(int i = 0; i < v.size(); i++)
-     v[i] = v[i] > t[i] ? 0 : v[i];
-}
